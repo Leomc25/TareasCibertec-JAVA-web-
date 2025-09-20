77c04c5e8e580dcdc9139f857390eace8812c320
Leomc25 <129339882+Leomc25@users.noreply.github.com>
2025-09-20 17:47:45 -0500

    Archivos

---
+++ javascriptfunciones.js
@@ -0,0 +1,9 @@
+// script.js
+function mostrarTelefono() {
+  Swal.fire({
+    title: 'Número de contacto',
+    text: '+(01) 452-3970',
+    icon: 'info',
+    confirmButtonText: 'Cerrar'
+  });
+}
